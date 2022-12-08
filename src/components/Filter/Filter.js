b5c1af04be36be661579f014deda6c87fafbd86e
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-12-08 23:53:17 +0200

    Create Filter.js

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { FilterIntput, FilterTitle } from './Filter.styled';
+
+export const Filter = ({ title, state, handleFindContact }) => {
+  const { filter } = state;
+  return (
+    <>
+      <FilterTitle>{title}</FilterTitle>
+      <FilterIntput
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={handleFindContact}
+      />
+    </>
+  );
+};
+
+Filter.propTypes = {
+  title: PropTypes.string.isRequired,
+  state: PropTypes.object.isRequired,
+  handleFindContact: PropTypes.func.isRequired,
+};
